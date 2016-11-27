9608227a18c29d5a20f9dac1ae025c31d843e97a
layfieldk <kevinjlayfield@gmail.com>
2016-11-27 10:53:18 -0500

    Chapter 6 of  tutorial completed

--- WorldForgingApi/wwwroot/js/@angular/upgrade/src/upgrade_adapter.js
+++ WorldForgingApi/wwwroot/js/@angular/upgrade/src/upgrade_adapter.js
@@ -54,7 +54,7 @@ var upgradeCount = 0;
  * ### Example
  *
  * ```
- * var adapter = new UpgradeAdapter(forwardRef(() => MyNg2Module));
+ * var adapter = new UpgradeAdapter(forwardRef(() => MyNg2Module), myCompilerOptions);
  * var module = angular.module('myExample', []);
  * module.directive('ng2Comp', adapter.downgradeNg2Component(Ng2Component));
  *
@@ -94,8 +94,9 @@ var upgradeCount = 0;
  * @stable
  */
 export var UpgradeAdapter = (function () {
-    function UpgradeAdapter(ng2AppModule) {
+    function UpgradeAdapter(ng2AppModule, compilerOptions) {
         this.ng2AppModule = ng2AppModule;
+        this.compilerOptions = compilerOptions;
         /* @internal */
         this.idPrefix = "NG2_UPGRADE_" + upgradeCount++ + "_";
         /* @internal */
@@ -346,17 +347,16 @@ export var UpgradeAdapter = (function () {
                     provide.decorator(NG1_TESTABILITY, [
                         '$delegate',
                         function (testabilityDelegate) {
-                            var _this = this;
                             var originalWhenStable = testabilityDelegate.whenStable;
+                            // Cannot use arrow function below because we need the context
                             var newWhenStable = function (callback) {
-                                var whenStableContext = _this;
-                                originalWhenStable.call(_this, function () {
+                                originalWhenStable.call(this, function () {
                                     var ng2Testability = moduleRef.injector.get(Testability);
                                     if (ng2Testability.isStable()) {
                                         callback.apply(this, arguments);
                                     }
                                     else {
-                                        ng2Testability.whenStable(newWhenStable.bind(whenStableContext, callback));
+                                        ng2Testability.whenStable(newWhenStable.bind(this, callback));
                                     }
                                 });
                             };
@@ -388,7 +388,7 @@ export var UpgradeAdapter = (function () {
                             ngDoBootstrap: function () { }
                         });
                         platformBrowserDynamic()
-                            ._bootstrapModuleWithZone(DynamicNgUpgradeModule, undefined, ngZone, function (componentFactories) {
+                            ._bootstrapModuleWithZone(DynamicNgUpgradeModule, _this.compilerOptions, ngZone, function (componentFactories) {
                             componentFactories.forEach(function (componentFactory) {
                                 var type = componentFactory.componentType;
                                 if (_this.upgradedComponents.indexOf(type) !== -1) {
@@ -405,7 +405,8 @@ export var UpgradeAdapter = (function () {
                             });
                         })
                             .then(resolve, reject);
-                    });
+                    })
+                        .catch(reject);
                 }
             ]);
         });
@@ -415,10 +416,12 @@ export var UpgradeAdapter = (function () {
         ngZone.run(function () { angular.bootstrap(element, [_this.idPrefix], config); });
         ng1BootstrapPromise = new Promise(function (resolve) {
             if (windowAngular.resumeBootstrap) {
-                var originalResumeBootstrap = windowAngular.resumeBootstrap;
+                var originalResumeBootstrap_1 = windowAngular.resumeBootstrap;
                 windowAngular.resumeBootstrap = function () {
-                    windowAngular.resumeBootstrap = originalResumeBootstrap;
-                    windowAngular.resumeBootstrap.apply(this, arguments);
+                    var _this = this;
+                    var args = arguments;
+                    windowAngular.resumeBootstrap = originalResumeBootstrap_1;
+                    ngZone.run(function () { windowAngular.resumeBootstrap.apply(_this, args); });
                     resolve();
                 };
             }
